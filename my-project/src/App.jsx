b2efc18cd3305f51c70d3b7efb3aecc5a387847e
Syed Raza Ali <razaali26296@gmail.com>
2024-03-09 17:47:38 +0530

    Third Commit

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl ">Hello world</h1>;
 }
 
 export default App;
--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Nav from "./components/Nav.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="bg-black text-white m-0 p-0">
+    <React.StrictMode>
+      <Nav />
+      <App />
+    </React.StrictMode>
+  </div>
+);
