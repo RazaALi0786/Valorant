442af51770ab8b905c11245187f8708800182839
Syed Raza Ali <razaali26296@gmail.com>
2024-03-25 21:46:28 +0530

    footer and nav part 3

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,8 @@ import { FaYoutube } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { FaDiscord } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa";
+import { SiValorant } from "react-icons/si";
+import { SiRiotgames } from "react-icons/si";
 
 const footer = () => {
   return (
@@ -10,23 +12,34 @@ const footer = () => {
       <div className="text-center bg-[#292929] text-sm text-white font-mono py-6">
         <p>THANKS FOR VISITING</p>
       </div>
-      <div className="bg-[#111111] text-white py-5 flex justify-center items-center gap-5 ">
-        <button className="bg-[#292929] p-1 rounded-md">
-          <FaTwitter />
+      <div className="bg-[#111111] text-white py-5 cursor-pointer">
+        <div className="flex items-center justify-center gap-5">
+          <button className="bg-[#292929] p-[6px] rounded-lg">
+            <FaTwitter />
+          </button>
+          <button className="bg-[#292929] p-[6px] rounded-lg">
+            <FaYoutube />
+          </button>
+          <button className="bg-[#292929] p-[6px] rounded-lg">
+            <FiInstagram />
+          </button>
+          <button className="bg-[#292929] p-[6px] rounded-lg">
+            <FaLinkedin />
+          </button>
+          <button className="bg-[#292929] p-[6px] rounded-lg">
+            <FaDiscord />
+          </button>
+        </div>
+      </div>
+      <div className="py-6 bg-[#111111] cursor-pointer flex items-center text-4xl justify-center gap-5">
+        <button className="text-gray-400 hover:text-white">
+          <SiRiotgames />
         </button>
-        <button className="bg-[#292929] p-1 rounded-md">
-          <FaYoutube />
-        </button>
-        <button className="bg-[#292929] p-1 rounded-md">
-          <FiInstagram />
-        </button>
-        <button className="bg-[#292929] p-1 rounded-md">
-          <FaLinkedin />
-        </button>
-        <button className="bg-[#292929] p-1 rounded-md">
-          <FaDiscord />
+        <button className="text-gray-400 hover:text-white">
+          <SiValorant />
         </button>
       </div>
+      <div></div>
     </>
   );
 };
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -137,7 +137,7 @@ function Nav() {
                 href="/about"
                 className="block px-3 py-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-red-700 md:p-0 dark:text-white md:dark:hover:text-red-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                ABOUT
+                ABOUT US
               </a>
             </li>
           </ul>
