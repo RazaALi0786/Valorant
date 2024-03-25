bf04c697eb0d305826dc792895725075d9a1648e
Syed Raza Ali <razaali26296@gmail.com>
2024-03-25 05:45:18 +0530

    modified button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <>
       <div className="text-center text-white bg-no-repeat bg-contain h-[100vh] bg-hero-pattern font-valorant relaitve">
-        <button className="absolute font-mono font-bold transform -translate-x-1/2 top-52 bg-[#ff4655] px-2 py-1 cursor-pointer aboslute left-1/2">
+        <button className="absolute font-mono font-bold transform -translate-x-1/2 top-52 bg-[#ff4655] px-2 py-1 cursor-pointer aboslute left-1/2 hover:bg-[#111111] border-b-4 border-red-800 hover:border-gray-800 rounded">
           What Is Valorant
         </button>
       </div>
