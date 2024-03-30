3cc8f2a63d243119157efe622be288c99a5561ed
Syed Raza Ali <razaali26296@gmail.com>
2024-03-30 15:14:37 +0530

    woorked on agents page

--- src/pages/Agents/Agents.jsx
+++ src/pages/Agents/Agents.jsx
@@ -39,7 +39,9 @@ const Agents = () => {
             <a
               href="#"
               className={`flex flex-col items-center bg-[#111111] border border-gray-700  shadow md:flex-row md:w-full hover:bg-black`}
-              style={{ background: `#${agent.backgroundGradientColors[2]}` }}
+              style={{
+                background: `linear-gradient(70deg, #${agent.backgroundGradientColors[0]}, #${agent.backgroundGradientColors[2]}, #${agent.backgroundGradientColors[1]}, #${agent.backgroundGradientColors[3]})`,
+              }}
             >
               <img
                 className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-72 md:rounded-none md:rounded-s-lg"
@@ -47,22 +49,19 @@ const Agents = () => {
                 alt="Gekko Image"
               />
               <div className="flex flex-col justify-between p-4 leading-normal">
-                <h3
-                  // className={`flex mb-2 text-2xl font-bold tracking-tight text-transparent font-valorant bg-clip-text bg-gradient-to-r bg-[#${agent?.backgroundGradientColors[0]}]`}
-                  className={`flex mb-2 text-2xl font-bold font-valorant text-[#${agent?.backgroundGradientColors[0]}]`}
-                  style={{ color: `#${agent.backgroundGradientColors[0]}` }}
-                >
+                <h3 className="flex mb-2 text-2xl font-bold tracking-tight font-valorant text-[#ece8e1]">
                   <img
-                    className="w-10 h-10 mr-4 -mt-2 border rounded-full border-lime-500"
+                    className="w-10 h-10 mr-4 -mt-2 border rounded-full "
+                    style={{
+                      border: `1px solid black`,
+                      background: `linear-gradient(#${agent.backgroundGradientColors[0]},#${agent.backgroundGradientColors[2]},#${agent.backgroundGradientColors[1]},#${agent.backgroundGradientColors[3]})`,
+                    }}
                     src={agent?.displayIcon}
                     alt=""
                   />
                   {agent?.displayName}
                 </h3>
-                <h5
-                  className="flex mb-2 text-lg font-bold tracking-tight text-transparent font-valorant bg-clip-text bg-gradient-to-r from-lime-400 to-green-700"
-                  style={{ color: `#${agent.backgroundGradientColors[1]}` }}
-                >
+                <h5 className="flex mb-2 text-lg font-bold tracking-tight font-valorant text-[#ece8e1]">
                   {agent?.role?.displayName}
                 </h5>
                 <p className="mb-3 font-normal text-[#ece8e1]">
